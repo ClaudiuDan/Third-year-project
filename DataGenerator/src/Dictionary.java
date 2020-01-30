bb01cc8351e2937eb869b9fdd926b338f8fc6a6c
dan c-i. (cid1u17) <cid1u17@soton.ac.uk>
2020-01-30 15:41:11 +0000

    add dictionary

--- DataGenerator/src/Dictionary.java
+++ DataGenerator/src/Dictionary.java
@@ -1,6 +1,23 @@
-public class Dictionary {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+public class Dictionary {
+    private static Map<String, List<String>> dictionary = new HashMap<>();
+    Dictionary() {
+        WordsRetrieval wordsRetrieval = new WordsRetrieval();
+        List<WordsRetrieval.Word> words = wordsRetrieval.getWords();
+        fillDictionary(words);
+    }
     public static String getRandomWord(String type) {
         return null;
+    }
+
+    private void fillDictionary(List<WordsRetrieval.Word> words) {
+        for (WordsRetrieval.Word word : words) {
+            dictionary.computeIfAbsent(word.type, (k) -> new ArrayList<>());
+            dictionary.get(word.type).add(word.value);
+        }
     }
 }
---
+++ DataGenerator/src/WordsRetrieval.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class WordsRetrieval {
+
+    private BufferedReader reader;
+
+    WordsRetrieval () {
+        try {
+            reader = new BufferedReader(new FileReader(new File("words")));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<Word> getWords () {
+        List<Word> words = new ArrayList<>();
+        String line;
+        try {
+            while ((line = reader.readLine()) != null) {
+                String[] split = line.split(" ");
+                words.add(new Word(split[0], split[1]));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return words;
+    }
+
+    class Word {
+        String type, value;
+
+        Word (String value, String type) {
+            this.type = type;
+            this.value = value;
+        }
+    }
+}
